a64fb3fddd6ab0be8bc9ff0710670f506e1de355
Simple <huyang1994822@gmail.com>
2025-02-05 14:48:47 +0800

    genKey

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import {useState} from "react";
 import "./App.css";
+import JSEncrypt from "jsencrypt";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [pubKey, setPubKey] = useState("");
+    const [priKey, setPriKey] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    function putPubKey(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setPubKey(event.target.value);
+    }
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    function putPriKey(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setPriKey(event.target.value);
+    }
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+    function genKey(){
+        const jse = new JSEncrypt();
+        const encrypt = jse.getKey();
+        setPubKey(encrypt.getPublicKey());
+        setPriKey(encrypt.getPrivateKey());
+    }
+    function checkKey(){
+        alert("check ok!")
+    }
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+    return (
+        <main className="w-full h-screen">
+            <h1 className="text-center h-12 text-4xl my-4">Qing Toolbox</h1>
+            <div className="flex flex-col items-center place-content-around">            
+                <textarea id="pub" onChange={putPubKey} value={pubKey}
+                className="w-2xl h-64 my-2 border border-solid"
+                />
+                <textarea id="pri" onChange={putPriKey} value={priKey}
+                className="w-2xl h-64 my-2 border border-solid"
+                />
+                <div className="flex flex-row">
+                    <button 
+                    className="mx-2 px-2 py-1 border border-solid border-blue-400 rounded-xl" 
+                    onClick={genKey}>生成RSA密钥</button>
+                    <button 
+                    className="mx-2 px-2 py-1 border border-solid border-blue-400 rounded-xl" 
+                    onClick={checkKey}>检查RSA密钥</button>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default App;
